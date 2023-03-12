7c3f7b89b2837a858a416c60d6b3aaa9c747cc9c
case-2 <case000002@gmail.com>
2023-03-12 17:06:27 +0530

    On branch master Your branch is up to date with 'mybrary/master'.
    Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-
-const indexRouter = require('./routes/index')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  }catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
+
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  })
+});
